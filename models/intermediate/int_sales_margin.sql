3ca997ad4b5c0921655e5a23295d100fefbb3c35
Leslie Zuta <lesliezutaq@gmail.com>
2024-08-27 21:35:02 +0000

    add sales_margin2

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,9 @@
+SELECT 
+    s.date_date,
+    ROUND(CAST(p.purchase_price AS FLOAT64)*s.quantity,2) AS purchase_cost,
+    ROUND(s.revenue - CAST(p.purchase_price AS FLOAT64)*s.quantity,2) AS margin 
+FROM {{ ref("stg_raw__sales") }} s
+LEFT JOIN {{ ref("stg_raw__product") }} p
+ON s.products_id = p.products_id
+ORDER BY date_date
+
