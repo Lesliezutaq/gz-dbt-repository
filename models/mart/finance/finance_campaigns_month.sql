53ee21fce98b4d54247bdd81df571f08df94c094
Leslie Zuta <lesliezutaq@gmail.com>
2024-08-28 21:31:49 +0000

    add final finance by month

--- models/mart/finance/finance_campaigns_month.sql
+++ models/mart/finance/finance_campaigns_month.sql
@@ -1,10 +1,9 @@
 SELECT 
     DATE_TRUNC(date_date, MONTH) AS datemonth,
-    ROUND(SUM(ads_margin),2) AS ads_margin,
-    ROUND(AVG(average_basket),2) AS average_basket,
-    ROUND(SUM(operational_margin),2) AS operational_margin,
+    ROUND(SUM(ads_margin),0) AS ads_margin,
+    ROUND(SUM(average_basket),1) AS average_basket,
+    ROUND(SUM(operational_margin),1) AS operational_margin,
     SUM(ads_cost) AS ads_cost,
-    SUM(ads_impression) AS ads_impression,
     SUM(ads_impression) AS ads_impression,
     SUM(ads_click) AS ads_click
 FROM {{ref("finance_campaigns_day")}} 
